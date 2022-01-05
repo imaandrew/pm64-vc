ea04b5b95d669684c818126b4b2ae1db971a3bdf
Andrew <14946103+imaandrew@users.noreply.github.com>
2022-01-04 20:08:10 -0500

    Split out ram functions

---
+++ asm/non_matchings/virtual_console/ram/ramGetControl16.s
@@ -0,0 +1,4 @@
+glabel ramGetControl16
+/* 800412DC 0003C7FC  38 60 00 00 */	li r3, 0
+/* 800412E0 0003C800  4E 80 00 20 */	blr 
+
---
+++ asm/non_matchings/virtual_console/ram/ramPutControl64.s
@@ -0,0 +1,4 @@
+glabel ramPutControl64
+/* 800412CC 0003C7EC  38 60 00 00 */	li r3, 0
+/* 800412D0 0003C7F0  4E 80 00 20 */	blr 
+
